5aec2a4f10961a5690d4f513d30dd8b9c6e3bbda
Dat Ngo <d.ngo@aswhiteglobal.com>
2020-03-28 21:51:40 +0700

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            var (x, y) = GetPoint(200, 300);
+            Console.WriteLine("x:" + x);
+            Console.WriteLine("y:" + y);
+
+            Console.ReadKey();
+
+        }
+
+        #region Tuples
+        public static (int x, int y) GetPoint(int x, int y)
+        {
+            return (x, y);
+        }
+        #endregion
+
+        abstract class Shape { };
+    }
+
+}
